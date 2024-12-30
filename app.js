049e954f37c70b877ca0fa57ef35fcc0c90fc0dc
CamiloCuenca <camilocuenca1810@gmail.com>
2024-12-29 21:23:34 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import express from 'express';
+
+
+const app = express();
+
+try{
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT,()=> console.log('server active on PORT:' + PORT))
+
+}catch(e){
+console.log(e)
+}
+
